99877dc9a87813bc06a4b5200b5aa941d7532866
Kate Gorbunova <47249365+kategorbunova@users.noreply.github.com>
2024-10-22 22:13:20 -0700

    draft of countdown timer and styled components

--- src/components/timers/Countdown.tsx
+++ src/components/timers/Countdown.tsx
@@ -1,3 +1,131 @@
-const Countdown = () => null;
+import {useState, useEffect, useRef} from 'react'
+import styled from "styled-components";
+import { Buttons, Button, Input, Inputs, TimerContainer, Timer } from '../../views/TimersView';
+
+const STATUS = {
+    STARTED: 'Started',
+    STOPPED: 'Stopped',
+  };
+
+function convertToSeconds(timeMinInput: number, timeSecInput: number) {
+    return parseInt(timeMinInput || '0') * 60 + parseInt(timeSecInput || '0');
+}
+
+const TimeDisplay = styled.div`
+  font-size: 2rem;
+  color: ${(props) => (props.isActive ? 'black' : 'grey')}; 
+`;
+
+const Countdown = () => {
+
+    const [timeMinInput, setTimeMinInput] = useState('')
+    const [timeSecInput, setTimeSecInput] = useState('')
+    const [status, setStatus] = useState(STATUS.STOPPED);
+    const [secondsRemaining, setSecondsRemaining] = useState(0)
+    const intervalRef = useRef(); 
+
+
+    const secondsOnTimer = secondsRemaining % 60
+    const minutesRemaining = (secondsRemaining - secondsOnTimer) / 60
+    const minutesOnTimer = minutesRemaining % 60
+    const hoursOnTimer = (minutesRemaining - minutesOnTimer) / 60
+  
+    const startCountdown = () => {
+        if (secondsRemaining === 0) {
+            const totalSeconds = convertToSeconds(timeMinInput, timeSecInput);
+            setTimeMinInput('');
+            setTimeSecInput('');
+            if (totalSeconds > 0) {
+              setSecondsRemaining(totalSeconds);
+            } else {
+              alert('Please enter a valid time.');
+              return;
+            }
+          }
+          setStatus(STATUS.STARTED); 
+        };
+
+    const stopCountdown = () => {
+      setStatus(STATUS.STOPPED);
+      clearInterval(intervalRef.current); 
+    }
+
+    const resetCountdown = () => {
+      stopCountdown();
+      setSecondsRemaining(0);
+      setTimeMinInput('');
+      setTimeSecInput('');
+    }
+
+    useEffect(() => {
+        if (status === STATUS.STARTED) {
+          intervalRef.current = setInterval(() => {
+            setSecondsRemaining((prev) => {
+              if (prev > 0) return prev - 1;
+              stopCountdown();
+              return 0;
+            });
+          }, 1000);
+        }
+    
+        return () => clearInterval(intervalRef.current); 
+      }, [status]);
+
+    return (
+        <div className="App">
+            <Inputs>
+            <Input>     
+                <input 
+                    style={{ width: '1rem', padding: '10px' }}
+                    id="timeMinInput" 
+                    value={timeMinInput}
+                    onChange={e=>{
+                        setTimeMinInput(e.target.value);
+                    }}/>
+                <div>min</div>
+            </Input>
+            <Input> 
+                <input 
+                    style={{ width: '1rem', padding: '10px' }}
+                    id="timeInput" 
+                    value={timeSecInput}
+                    onChange={e=>{
+                        setTimeSecInput(e.target.value);
+                    }}/>
+                    <div>sec</div>
+            </Input>
+            </Inputs>
+            <Buttons>
+                <Button 
+                    onClick={startCountdown} 
+                    //type="button" 
+                    style={{backgroundColor: 'darkgreen'}}>
+                    Start
+                </Button>
+                <Button 
+                    onClick={stopCountdown} 
+                    //type="button" 
+                    style={{backgroundColor: 'darkred'}}>
+                    Stop
+                </Button>
+                <Button 
+                    onClick={resetCountdown} 
+                    //type="button" 
+                    style={{backgroundColor: 'grey'}}>
+                    Reset
+                </Button>
+            </Buttons>
+            <Timer>
+            <TimeDisplay isActive={status === STATUS.STARTED}>
+                {String(hoursOnTimer).padStart(2, '0')}:
+                {String(minutesOnTimer).padStart(2, '0')}:
+                {String(secondsOnTimer).padStart(2, '0')}
+             </TimeDisplay>
+            
+            </Timer>
+        </div>
+      )
+};
+
 
 export default Countdown;
--- src/views/TimersView.tsx
+++ src/views/TimersView.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import {useState, useEffect, useRef} from 'react'
+
 
 import Stopwatch from "../components/timers/Stopwatch";
 import Countdown from "../components/timers/Countdown";
@@ -7,20 +9,80 @@ import Tabata from "../components/timers/Tabata";
 
 const Timers = styled.div`
   display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: space-between;
+  gap: 2rem;
+`;
+
+const TimerTitle = styled.div`
+  font-size: 2em;
+  font-family: Georgia, serif;
+  color: #23303d;
+  text-align: center;
+  padding: 1rem;
+`;
+
+const TimerContainer = styled.div`
+  display: flex;
   flex-direction: column;
-  align-items: center;
+  text-align: center;
+  justify-content: center;
+  align-content: space-between;
+  background-color: lightgrey;
+    border-radius: 10px;
 `;
 
 const Timer = styled.div`
-  border: 1px solid gray;
-  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  align-content: space-between;
+  padding: 50px;
   margin: 10px;
-  font-size: 1.5rem;
+  font-size: 3rem;
+  width: 15rem;
+  height: 5rem;
+  background-color: #efefef;
+  border-radius: 10px;
+  color: #935430;
+  font-family: "Gill Sans", sans-serif;
+  font-variant: small-caps;
 `;
 
-const TimerTitle = styled.div``;
+const Button = styled.button`
+color: white;
+font-size: 1em;
+margin: 0.25em;
+padding: 1em 1em;
+border-radius: 100px;
+border: 2px solid white;
+`
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: space-between;
+  padding-top: 1rem;
+`
+
+const Input = styled.div`
+  font-size: 1em;
+  font-family: "Gill Sans", sans-serif;
+  color: #23303d;
+`;
+
+const Inputs = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: space-between;
+  gap: 2rem;
+`;
 
 const TimersView = () => {
+
   const timers = [
     { title: "Stopwatch", C: <Stopwatch /> },
     { title: "Countdown", C: <Countdown /> },
@@ -31,13 +93,17 @@ const TimersView = () => {
   return (
     <Timers>
       {timers.map((timer) => (
-        <Timer key={`timer-${timer.title}`}>
+        <div key={`timer-${timer.title}`}>
+          <TimerContainer>
           <TimerTitle>{timer.title}</TimerTitle>
-          {timer.C}
-        </Timer>
+          <div>{timer.C}</div>
+          </TimerContainer>
+        </div>
       ))}
     </Timers>
+  
   );
 };
 
 export default TimersView;
+export {Button, Buttons, Input, Inputs, TimerContainer, Timer};
