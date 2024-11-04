c5fe37255de7e0d61cafa51d896bd22ce56a6496
Kate Gorbunova <47249365+kategorbunova@users.noreply.github.com>
2024-11-04 08:28:39 -0800

    path updated for import

--- src/views/TimersView.tsx
+++ src/views/TimersView.tsx
@@ -1,30 +1,27 @@
+import { TimerContainer, Timers } from '../utils/styles';
 
-import {  TimerContainer, Timers } from '/Users/kategorbunova/Library/Mobile Documents/com~apple~CloudDocs/Personal/Personal - HES School/JS React/assignment-1-kategorbunova/src/utils/styles.tsx'
-
-import Stopwatch from "../components/timers/Stopwatch";
-import Countdown from "../components/timers/Countdown";
-import XY from "../components/timers/XY";
-import Tabata from "../components/timers/Tabata";
+import Countdown from '../components/timers/Countdown';
+import Stopwatch from '../components/timers/Stopwatch';
+import Tabata from '../components/timers/Tabata';
+import XY from '../components/timers/XY';
 
 const TimersView = () => {
+    const timers = [
+        { title: 'Stopwatch', C: <Stopwatch /> },
+        { title: 'Countdown', C: <Countdown /> },
+        { title: 'XY', C: <XY /> },
+        { title: 'Tabata', C: <Tabata /> },
+    ];
 
-  const timers = [
-    { title: "Stopwatch", C: <Stopwatch /> },
-    { title: "Countdown", C: <Countdown /> },
-    { title: "XY", C: <XY /> },
-    { title: "Tabata", C: <Tabata /> },
-  ];
-
-  return (
-    <Timers>
-      {timers.map((timer) => (
-        <div key={`timer-${timer.title}`}>
-          <TimerContainer> {timer.C} </TimerContainer>
-        </div>
-      ))}
-    </Timers>
-  
-  );
+    return (
+        <Timers>
+            {timers.map(timer => (
+                <div key={`timer-${timer.title}`}>
+                    <TimerContainer> {timer.C} </TimerContainer>
+                </div>
+            ))}
+        </Timers>
+    );
 };
 
 export default TimersView;
